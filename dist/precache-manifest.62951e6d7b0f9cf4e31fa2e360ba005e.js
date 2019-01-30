c052d22da3cf6f72b2ab133cf5bb4a60ddaadd84
yong <zengjunyong@gmail.com>
2019-01-30 19:53:41 +0800

    dist

---
+++ dist/precache-manifest.62951e6d7b0f9cf4e31fa2e360ba005e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1fb1d28191ebb4b93d7a",
+    "url": "/js/chunk-vendors.6dfe03e2.js"
+  },
+  {
+    "revision": "bc0fbd5470e83cb55111",
+    "url": "/js/app.00ad21ef.js"
+  },
+  {
+    "revision": "ba8156be8b1b5bd4f19a",
+    "url": "/js/about.0d02e657.js"
+  },
+  {
+    "revision": "0a0d83f162da753ec6d403a0e40ae5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bc0fbd5470e83cb55111",
+    "url": "/css/app.84fe17e1.css"
+  }
+];
